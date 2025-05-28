0f6c30e3aa5e6cfa8e8fc632b8e54bf5e0ccaf45
Heitor <heitorschleder33@gmail.com>
2025-05-28 15:06:39 -0300

    adding metas

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+useHead({
+  title: 'Dr. Solar Clean',
+  meta: [
+    { name: 'description', content: 'Veja os resultados reais dos nossos serviços de limpeza e transformação.' },
+    { property: 'og:title', content: 'Dr. Solar Clean' },
+    { property: 'og:description', content: 'Veja os resultados reais dos nossos serviços de limpeza e transformação.' },
+    {
+      property: 'og:image',
+      content: 'https://i.imgur.com/UvAWsrr.png'
+    },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://drsolarclean.com.br' },
+    { name: 'twitter:card', content: 'summary_large_image' }
+  ]
+})
+</script>
 <template>
   <div>
     <NuxtPage />
